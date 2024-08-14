2db22921c2519b596412cd4f47fc57b102034720
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2024-08-14 15:59:32 +0400

    add flake8

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -21,3 +21,15 @@ def users():
 @app.route('/courses/<id>')
 def courses(id):
     return f'Course id: {id}'
+
+
+@app.route('/users/<id>')
+def users(id):
+    user = {
+        "id": id,
+        "name": f"user-{id}"
+    }
+    return render_template(
+        'users/show.html',
+        user=user,
+    )
