515bb43d32f57a0f857a919d5adf1ba740602ea0
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2024-08-14 18:02:17 +0400

    init lesson post-form

--- example.py
+++ example.py
@@ -1,14 +1,10 @@
-from flask import Flask, render_template, request
+import json
+import uuid
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-users = [
-    {'id': 1, 'name': 'mike'},
-    {'id': 2, 'name': 'mishel'},
-    {'id': 3, 'name': 'adel'},
-    {'id': 4, 'name': 'keks'},
-    {'id': 5, 'name': 'kamila'}
-]
+users = json.load(open("./users.json", 'r'))
 
 
 @app.route('/')
@@ -17,9 +13,10 @@ def hello_world():
 
 
 @app.route('/users/')
-def get_users():
+def users_get():
+    with open("./users.json", "r") as f:
+        users = json.load(f)
     term = request.args.get('term', '')
-    print(users)
     filtered_users = [user for user in users if term in user['name']]
     return render_template(
         'users/index.html',
@@ -29,8 +26,47 @@ def get_users():
 
 
 @app.post('/users')
-def post_users():
-    return 'Users', 302
+def users_post():
+    user_data = request.form.to_dict()
+    errors = validate(user_data)
+    if errors:
+        return render_template(
+            'users/new.html',
+            user=user_data,
+            errors=errors,
+        )
+    id = str(uuid.uuid4())
+    user = {
+        'id': id,
+        'name': user_data['name'],
+        'email': user_data['email']
+    }
+    users.append(user)
+    with open("./users.json", "w") as f:
+        json.dump(users, f)
+    return redirect('/users', code=302)
+
+
+@app.route('/users/new')
+def users_new():
+    user = {'name': '', 'email': ''}
+    errors = {}
+    return render_template(
+        'users/new.html',
+        user=user,
+        errors=errors,
+    )
+
+
+@app.route('/users/<id>')
+def show_user(id):
+    with open("./users.json", "r") as f:
+        users = json.load(f)
+    user = next(user for user in users if id == str(user['id']))
+    return render_template(
+        'users/show.html',
+        user=user,
+    )
 
 
 @app.route('/courses/<id>')
@@ -38,13 +74,10 @@ def courses(id):
     return f'Course id: {id}'
 
 
-@app.route('/users/<id>')
-def show_user(id):
-    user = {
-        "id": id,
-        "name": f"user-{id}"
-    }
-    return render_template(
-        'users/show.html',
-        user=user,
-    )
+def validate(user):
+    errors = {}
+    if not user['name']:
+        errors['name'] = "Can't be blank"
+    if not user['email']:
+        errors['email'] = "Can't be blank"
+    return errors
