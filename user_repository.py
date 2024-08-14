c74bfe42fc459a7c35672bd80ffe65f5dabf4cb1
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2024-08-14 21:05:49 +0400

    init lesson crud-delete

--- user_repository.py
+++ user_repository.py
@@ -2,8 +2,7 @@ import json
 import sys
 import uuid
 
-
-class UserRepository():
+class UserRepository:
     def __init__(self):
         self.users = json.load(open("./users.json", 'r'))
 
@@ -13,27 +12,36 @@ class UserRepository():
     def find(self, id):
         try:
             for user in self.users:
-                if id == str(user['id']):
+                if str(id) == str(user['id']):
                     return user
         except KeyError:
             sys.stderr.write(f'Wrong post id: {id}')
             raise
 
-    def save(self, new_user):
+    def save(self, user_data):
         # repository should know nothing about validation in outer layer
-        if not (new_user.get('name') and new_user.get('email')):
-            raise Exception(f'Wrong data: {json.loads(new_user)}')
-        # replace already existed user
-        if new_user.get('id'):
-            for current in self.users:
-                if new_user['id'] == current['id']:
-                    del current
-                    break
-            self.users.append(new_user)
-        # or add new
-        else:
+        if not (user_data.get('name') and user_data.get('email')):
+            raise Exception(f'Wrong data: {json.dumps(user_data)}')
+
+        if 'id' not in user_data:
+            new_user = user_data.copy()
             new_user['id'] = str(uuid.uuid4())
             self.users.append(new_user)
+        else:
+            for i, existing_user in enumerate(self.users):
+                if str(user_data['id']) == str(existing_user['id']):
+                    self.users[i] = user_data
+                    break
+            else:
+                raise Exception(f"User with id {user_data['id']} not found")
+
         with open("./users.json", "w") as f:
             json.dump(self.users, f)
-        return new_user['id']
+
+        return user_data['id']
+
+    def destroy(self, id):
+        current_user = self.find(id)
+        self.users.remove(current_user)
+        with open("./users.json", "w") as f:
+            json.dump(self.users, f)
