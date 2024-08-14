0d2ff48c7ef16e81fc4b4dab7b0e1973be49d65c
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2024-08-14 15:32:43 +0400

    init lesson handlers

--- example.py
+++ example.py
@@ -2,6 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Welcome to Flask!'
+
+
+@app.get('/users')
+def users_get():
+    return 'GET /users'
+
+
+@app.post('/users')
+def users_post():
+    return 'POST /users'
